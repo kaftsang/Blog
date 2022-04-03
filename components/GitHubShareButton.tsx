2b81a58a98121db5b65b4bae6b687c58d157abe8
Shen30000 <188976656@qq.com>
2022-04-04 00:23:46 +0800

    icon

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://github.com/Shen30000/Blog'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
